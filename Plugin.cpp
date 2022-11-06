7bd43575c59110c6280b2da8b47fa818704d5f37
Fionn <fionnsperath@outlook.de>
2022-11-06 13:47:04 +0100

    implemented functionality

--- Plugin.cpp
+++ Plugin.cpp
@@ -1,20 +1,66 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "Plugin.h"
 #include <string>
+#include <set>
 
 using namespace std;
 
+set<string> withMe = {};
+string pluginName = MY_PLUGIN_NAME;
+
 ESPlugin::ESPlugin() : EuroScopePlugIn::CPlugIn(EuroScopePlugIn::COMPATIBILITY_CODE, MY_PLUGIN_NAME, MY_PLUGIN_VERSION, MY_PLUGIN_DEVELOPER, MY_PLUGIN_COPYRIGHT)
-{
-	string pluginName = MY_PLUGIN_NAME;
+{	
 	string version = MY_PLUGIN_VERSION;
 	DisplayUserMessage(pluginName.c_str(), "Initialisation", ("Version " + version + " loaded").c_str(), true, false, false, false, false);
 
-
+	RegisterTagItemType("With Me", TAG_ITEM_WITHME);
+	RegisterTagItemFunction("Toggle With Me", TAG_FUNC_TOGGLE_WITHME);
 }
 
 ESPlugin::~ESPlugin()
+{}
+
+void ESPlugin::OnGetTagItem(CFlightPlan flightPlan, CRadarTarget RadarTarget, int ItemCode, int TagData, char sItemString[16], int* pColorCode, COLORREF* pRGB, double* pFontSize)
 {
+	string callsign = _strdup(flightPlan.GetCallsign());
+	if (ItemCode == TAG_ITEM_WITHME) {
+		string display = "\xac";
 
+		if (IsWithMe(callsign)) {
+			display = "\xa4";
+		}
+		
+		strcpy_s(sItemString, sizeof sItemString, display.c_str());
+	}
+}
 
+void ESPlugin::OnFunctionCall(int FunctionId, const char* sItemString, POINT Pt, RECT Area)
+{
+	CFlightPlan flightPlan = FlightPlanSelectASEL();
+	char* callsign = _strdup(flightPlan.GetCallsign());
+	if (FunctionId == TAG_FUNC_TOGGLE_WITHME) {
+		ToggleWithMe(callsign);
+	}
+}
+
+bool ESPlugin::IsWithMe(string callsign)
+{
+	bool isWithMe = withMe.count(callsign) != 0;
+	
+	return isWithMe;
+}
+
+void ESPlugin::ToggleWithMe(string callsign)
+{
+	if (IsWithMe(callsign)) {
+		withMe.erase(callsign);
+	}
+	else {
+		withMe.insert(callsign);
+	}
+
+	string str = "";
+	for (set<string>::iterator it = withMe.begin(); it != withMe.end(); ++it) {
+		str = str + *it + " ";
+	}
 }
--- Plugin.h
+++ Plugin.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <EuroScopePlugIn.h>
 
+#include<string>
+
 
 using namespace std;
 using namespace EuroScopePlugIn;
@@ -10,6 +12,8 @@ using namespace EuroScopePlugIn;
 #define MY_PLUGIN_DEVELOPER "Fionn Sperath <fionn@fsperath.de>"
 #define MY_PLUGIN_COPYRIGHT "GPL v3"
 
+const int TAG_ITEM_WITHME = 100;
+const int TAG_FUNC_TOGGLE_WITHME = 101;
 
 class ESPlugin : public EuroScopePlugIn::CPlugIn
 {
@@ -20,4 +24,10 @@ public:
 	ESPlugin();
 	virtual ~ESPlugin();
 
+	void OnGetTagItem(CFlightPlan flightPlan, CRadarTarget RadarTarget, int ItemCode, int TagData, char sItemString[16], int* pColorCode, COLORREF* pRGB, double* pFontSize);
+
+	void OnFunctionCall(int FunctionId, const char* sItemString, POINT Pt, RECT Area);
+
+	bool IsWithMe(string callsign);
+	void ToggleWithMe(string callsign);
 };
